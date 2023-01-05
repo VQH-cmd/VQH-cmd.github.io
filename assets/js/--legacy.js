3207e3fa525a0af53faf4a1f62c4600910cf045d
VQH-cmd <vqh.cmd@gmail.com>
2023-01-05 14:58:50 +0700

    .

--- assets/js/--legacy.js
+++
@@ -1,9 +0,0 @@
-function $__ie() {
-	var isIE = /*@cc_on!@*/false || !!document.documentMode;
-	var $loader = document.getElementById("loader");
-	var $ie = document.getElementById("ie");
-	if (isIE) {
-		$loader.classList.add("q1");
-		$ie.classList.add("q1");
-	}
-}; $__ie();
